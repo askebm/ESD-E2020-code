2ef3bb82df84e6eceee404174c1b21a26cf59062
Victor Staven <victorstaven@protonmail.com>
2020-12-02 17:00:11 +0100

    include numpad

---
+++ include/driver/NumpadDriver/NumpadDriver.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <array>
+#include <string>
+#include "include/ESD/driver/GPIO.hpp"
+
+class NumpadDriver
+{
+public:
+    NumpadDriver();
+    ~NumpadDriver();
+    // char readMatrix();
+
+    int check();
+    int getValue();
+    bool init();
+
+private:
+    std::array<GPIO*,4> rows;
+    std::array<GPIO*,4> cols;
+    int matrix_layout[4][4] = {
+        {1,2,3,10},
+        {4,5,6,11},
+        {7,8,9,12},
+        {0,15,14,13}
+    };
+
+};
