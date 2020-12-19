e8811c65a4b32ba40c1e56795d84bd9b86f2fd21
Aske Bækdal Møller <askebm@live.dk>
2020-12-19 19:15:50 +0100

    Button now accepts lambda function

--- include/ESD/gui/Button.hpp
+++ include/ESD/gui/Button.hpp
@@ -13,6 +13,7 @@ class Button : public GraphicsElement
 		SDL_Rect rectangle;
 		SDL_Color color;
 		SDL_Texture* texture;
+		void (*callback)();
 	public:
 		void render(SDL_Renderer* r);
 		void process(SDL_Event* e);
@@ -21,22 +22,22 @@ class Button : public GraphicsElement
 
 		// Default for diffrent sizes
 		static inline Button* Small(const int& x, const int& y, const SDL_Color& c,SDL_Renderer* ren,
-				const std::string& s = std::string("Small")){
+				void (*callback)() = nullptr, const std::string& s = std::string("Small")){
 			SDL_Rect r{ .x = x, .y = y , .w = 128, .h=128};
 			return new Button(r,c,s,ren);
 		}
 		static inline Button* Medium(const int& x, const int& y, const SDL_Color& c,SDL_Renderer* ren,
-				std::string s = std::string("Medium")){
+				void (*callback)() = nullptr, std::string s = std::string("Medium")){
 			SDL_Rect r{ .x = x, .y = y , .w = 256, .h=128};
 			return new Button(r,c,s,ren);
 		}
 		static inline Button* Big(const int& x, const int& y, const SDL_Color& c, SDL_Renderer* ren,
-				std::string s = std::string("Big")){
+				void (*callback)() = nullptr, std::string s = std::string("Big")){
 			SDL_Rect r{ .x = x, .y = y , .w = 256, .h=256};
 			return new Button(r,c,s,ren);
 		}
 
-		Button(const SDL_Rect&,const SDL_Color&, const std::string&, SDL_Renderer*);
+		Button(const SDL_Rect&,const SDL_Color&, const std::string&, SDL_Renderer*, void (*callback)() = nullptr);
 		~Button();
 };
 
