387805f0d19d97320c509b09677233dbdcd663ee
Marc Evald <marc.evald@gmail.com>
2021-01-15 23:12:20 +0100

    Removed marc stuff

--- cmake/zybo-toolchain.cmake
+++ cmake/zybo-toolchain.cmake
@@ -4,8 +4,6 @@ set(CMAKE_SYSTEM_PROCESSOR          arm)
 # Without that flag CMake is not able to pass test compilation check
 set(CMAKE_TRY_COMPILE_TARGET_TYPE   STATIC_LIBRARY)
 
-#set(CMAKE_SYSROOT /home/marc/zybo)
-
 set(CMAKE_AR                        ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf-ar${CMAKE_EXECUTABLE_SUFFIX})
 set(CMAKE_ASM_COMPILER              ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf-gcc${CMAKE_EXECUTABLE_SUFFIX})
 set(CMAKE_C_COMPILER                ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf-gcc${CMAKE_EXECUTABLE_SUFFIX})
@@ -15,8 +13,6 @@ set(CMAKE_OBJCOPY                   ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gn
 set(CMAKE_RANLIB                    ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf-ranlib${CMAKE_EXECUTABLE_SUFFIX} CACHE INTERNAL "")
 set(CMAKE_SIZE                      ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf-size${CMAKE_EXECUTABLE_SUFFIX} CACHE INTERNAL "")
 set(CMAKE_STRIP                     ${BAREMETAL_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf-strip${CMAKE_EXECUTABLE_SUFFIX} CACHE INTERNAL "")
-
-#set(CMAKE_FIND_ROOT_PATH "/home/marc/zybo/usr/lib;/home/marc/zybo/usr/include;${CMAKE_FIND_ROOT_PATH}")
 
 #set(CMAKE_C_FLAGS                   "-Wno-psabi --specs=nosys.specs -fdata-sections -ffunction-sections -Wl,--gc-sections" CACHE INTERNAL "")
 #set(CMAKE_CXX_FLAGS                 "${CMAKE_C_FLAGS} -fno-exceptions" CACHE INTERNAL "")
